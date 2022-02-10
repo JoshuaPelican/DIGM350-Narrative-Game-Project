3d8f4bcf0790b7d6fd688fb002195796e1338147
JoshuaPelkington <65318134+JoshuaPelican@users.noreply.github.com>
2022-02-09 22:41:52 -0500

    Setup Alot of Conversation Starters
    - Tortoise Intro
    - Justin Beaver
    - Raccoon
    - Sloth
    - Need to Fix Barry + Do Ending

---
+++ Tortoise and the Hare/Assets/Scripts/Encounters/ConditionEventTrigger.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class ConditionEventTrigger : MonoBehaviour
+{
+    [TitleGroup("Trigger Settings")]
+    [ValueDropdown("GetAllConditions", FlattenTreeView = true, DropdownTitle = "Select A Condition")]
+    [SerializeField] Condition[] Conditions;
+    [Space]
+    [ValueDropdown("GetAllEvents", FlattenTreeView = true, DropdownTitle = "Select An Event")]
+    [SerializeField] EventVariable eventToTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        if (!Conditions.All(x => x.Value == true) && Conditions.Length > 0)
+            return;
+
+        eventToTrigger.OnInvoke.Invoke();
+
+        Destroy(gameObject);
+    }
+
+    static IEnumerable GetAllConditions()
+    {
+        return AssetDatabase.FindAssets("t:condition", new[] { "Assets/Dialogue" })
+            .Select(x => AssetDatabase.GUIDToAssetPath(x))
+            .Select(x => new ValueDropdownItem(AssetDatabase.LoadAssetAtPath<Condition>(x).name, AssetDatabase.LoadAssetAtPath<Condition>(x)));
+    }
+
+    static IEnumerable GetAllEvents()
+    {
+        return AssetDatabase.FindAssets("t:eventvariable", new[] { "Assets/Variables" })
+            .Select(x => AssetDatabase.GUIDToAssetPath(x))
+            .Select(x => new ValueDropdownItem(AssetDatabase.LoadAssetAtPath<EventVariable>(x).name, AssetDatabase.LoadAssetAtPath<EventVariable>(x)));
+    }
+}
